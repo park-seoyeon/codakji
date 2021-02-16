6119e8f16cb9f70e515a53f7461e51536bd40213
송현아 <sohyua2@naver.com>
2021-02-16 20:11:30 +0900

    backend server 404 config edit(RootController)

--- backend_intergrated/src/main/java/io/openvidu/js/java/controller/EmailController.java
+++ backend_intergrated/src/main/java/io/openvidu/js/java/controller/EmailController.java
@@ -62,7 +62,8 @@ public class EmailController {
 				String htmlStr = "<div style='text-align: center; margin-top:10%;'><h2>성공적으로 인증되었습니다.</h2>"
 						+"<img src = 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F7AVpo%2FbtqUM75u9KM%2F6a6GmXy384zwyq8sdZgLQK%2Fimg.png'/>"
 						+"<br>"
-						+"<a href='http://localhost:8080/' style='text-decoration:none'><h2 style='color:blue'>코딱지로 이동하기</h2></a></div>";
+						//+"<a href='https://i4a203.p.ssafy.io:8000' style='text-decoration:none'><h2 style='color:blue'>코딱지로 이동하기</h2></a></div>";
+				+"<a href='http://localhost:8080/' style='text-decoration:none'><h2 style='color:blue'>코딱지로 이동하기</h2></a></div>";
 				return new ResponseEntity<String>(htmlStr, HttpStatus.OK);
 			}
 		} catch (Exception e) {
