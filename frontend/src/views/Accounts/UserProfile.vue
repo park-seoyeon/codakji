93b949c1498b62ddd3f7910f53f555d8086f51fc
jun9395 <jun9395@naver.com>
2021-02-17 17:00:27 +0900

    notice_crud_blueprint

--- frontend/src/views/Accounts/UserProfile.vue
+++ frontend/src/views/Accounts/UserProfile.vue
@@ -31,7 +31,7 @@
       <!-- 내 정보 보기 -->
       <v-tab-item>
         <template style="text_align: center">
-          <v-card class="mx-auto my-10" max-width="80%" min-height="400px">
+          <v-card class="mx-auto py-15" max-width="80%" min-height="400px">
             <img :src="userInfo.profileImg" width="10%" style="margin-top: 50px" />
             <v-card-text style="font-size:1.5em">
               <div class="text--primary">
@@ -75,6 +75,7 @@
                       type="password"
                       v-model="password"
                       prepend-icon="mdi-lock"
+                      @keypress.enter="checkPassword"
                     ></v-text-field>
                     <v-card-actions>
                       <v-spacer></v-spacer>
@@ -88,8 +89,23 @@
                   </v-card>
                 </v-dialog>
               </v-row>
+              <!-- <v-btn v-if="isAdmin" color="primary">
+                <v-icon color="white">mdi-bell-plus-outline</v-icon>
+                공지 추가하기
+              </v-btn> -->
             </template>
-            <br /><br />
+            <br>
+            <v-btn
+              color="primary"
+              @click="
+                isUpdate = false;
+                editNotice = true;
+              "
+              v-if="isAdmin"
+            >
+              <v-icon color="white">mdi-bell-plus-outline</v-icon>
+              공지 추가하기
+            </v-btn>
 
             <!-- 정보 수정 페이지 -->
             <v-expand-transition>
@@ -98,59 +114,59 @@
                 class="transition-fast-in-fast-out v-card--reveal"
                 style="height: 100%"
               >
-                <br /><br />
-
                 <v-row>
                   <v-col cols="2"></v-col>
                   <v-col cols="4">
-                    <img
-                      :src="userInfo.profileImg"
-                      width="170px"
-                      style="border: 3px solid gold; border-radius: 10px"
-                    />
-                    <v-file-input
-                      accept="image/*"
-                      label="프로필 사진 수정하기"
-                      filled
-                      prepend-icon="mdi-pencil"
-                      id="photo"
-                      enctype="multipart/form-data"
-                      @change="Preview_image"
-                      v-model="image"
-                    ></v-file-input>
-                    <br />
+                    <div class="my-10">
+                      <img
+                        :src="userInfo.profileImg"
+                        width="170px"
+                        style="border: 3px solid gold; border-radius: 10px"
+                      />
+                      <v-file-input
+                        accept="image/*"
+                        label="프로필 사진 수정하기"
+                        filled
+                        prepend-icon="mdi-pencil"
+                        id="photo"
+                        enctype="multipart/form-data"
+                        @change="Preview_image"
+                        v-model="image"
+                      ></v-file-input>
+                    </div>
                   </v-col>
                   <v-col cols="4">
-                    <v-textarea
-                      label="이름"
-                      auto-grow
-                      outlined
-                      rows="1"
-                      row-height="15"
-                      v-model="newName"
-                    ></v-textarea>
-                    <v-text-field
-                      type="password"
-                      :rules="passwordRules"
-                      label="비밀번호"
-                      auto-grow
-                      outlined
-                      rows="1"
-                      row-height="15"
-                      v-model="newPassword"
-                    ></v-text-field>
-                    <v-text-field
-                      type="password"
-                      label="비밀번호 확인"
-                      auto-grow
-                      outlined
-                      rows="1"
-                      row-height="15"
-                      v-model="newPassword2"
-                    ></v-text-field>
+                    <div class="my-10">
+                      <v-textarea
+                        label="이름"
+                        auto-grow
+                        outlined
+                        rows="1"
+                        row-height="15"
+                        v-model="newName"
+                      ></v-textarea>
+                      <v-text-field
+                        type="password"
+                        :rules="passwordRules"
+                        label="비밀번호"
+                        auto-grow
+                        outlined
+                        rows="1"
+                        row-height="15"
+                        v-model="newPassword"
+                      ></v-text-field>
+                      <v-text-field
+                        type="password"
+                        label="비밀번호 확인"
+                        auto-grow
+                        outlined
+                        rows="1"
+                        row-height="15"
+                        v-model="newPassword2"
+                      ></v-text-field>
+                    </div>
                   </v-col>
                 </v-row>
-                <br />
 
                 <v-btn class="ma-2" color="orange darken-2" dark @click="editCancel">
                   <v-icon dark left> mdi-arrow-left </v-icon>돌아가기
@@ -160,14 +176,105 @@
                   <v-icon dark> mdi-wrench </v-icon>수정하기
                 </v-btn>
 
-                <v-card-text class="mt-10" style="cursor: pointer" @click="deleteUser"
-                  >| 회원탈퇴 |</v-card-text
-                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <span
+                    class="mt-10"
+                    style="cursor: pointer; color: darkgrey;"
+                    @click="deleteUser"
+                  >
+                    | 회원탈퇴 |
+                  </span>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
               </v-card>
             </v-expand-transition>
+
+
+            <!-- 공지 추가/수정 하기 모달창 -->
+            <v-dialog v-model="editNotice" width="50%">
+              <v-card min-height="500px" style="position: relative">
+                <v-card-title class="headline yellow lighten-2">
+                  <img src="@/assets/img/codackji_logo.png" height="30px" />
+                </v-card-title>
+
+                <v-select
+                  :items="numbers"
+                  label="새로 작성"
+                  dense
+                  solo
+                  max-width="10px"
+                  v-model="selected_number"
+                ></v-select>
+
+                <v-card-text style="font-size:1.2em;">
+                  <br />
+                  <v-textarea
+                    label="제목"
+                    auto-grow
+                    outlined
+                    rows="1"
+                    row-height="10"
+                    v-model="editNoticeTitle"
+                  ></v-textarea>
+                  <v-textarea
+                    label="내용"
+                    auto-grow
+                    outlined
+                    rows="10"
+                    row-height="40"
+                    v-model="editNoticeContent"
+                  ></v-textarea>
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn text @click="cancelNotice" style="font-size:1.4em">
+                    <v-icon color="#FFB911" size="20">mdi-close-thick</v-icon>
+                    취소하기
+                  </v-btn>
+                  <v-btn
+                    v-show="isUpdate"
+                    text
+                    @click="deleteNotice"
+                    style="font-size:1.4em"
+                  >
+                    <v-icon color="#555555" size="20">mdi-delete-outline</v-icon>
+                    삭제하기
+                  </v-btn>
+                  <v-btn
+                    v-show="!isUpdate"
+                    text
+                    @click="uploadNotice"
+                    style="font-size:1.4em"
+                  >
+                    <v-icon color="#FFB911" size="20">mdi-lead-pencil</v-icon>
+                    등록하기
+                  </v-btn>
+                  <v-btn
+                    v-show="isUpdate"
+                    text
+                    @click="updateNotice"
+                    style="font-size:1.4em"
+                  >
+                    <v-icon color="#FFB911" size="20">mdi-lead-pencil</v-icon>
+                    수정하기
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-card>
           <br />
         </template>
+        <v-chip
+          v-if="isAdmin"
+          outlined
+          @click="$router.push({ name: 'NoticeAdminPage'})"
+        >
+          공지사항 수정 페이지
+        </v-chip>
       </v-tab-item>
 
       <!-- 내가 푼 문제 목록 -->
@@ -206,6 +313,7 @@ export default {
       },
       image: '',
       tabs: null,
+      isAdmin: false,
       reveal: false,
       dialog: false,
       isSourceCode: false,
@@ -221,6 +329,13 @@ export default {
           /^(?=.*[a-z])(?=.*[0-9]).{8,16}$/.test(v) ||
           '소문자, 숫자를 포함한 8-16자로 입력해 주세요',
       ],
+      notices: '',
+      numbers: ['새로 작성'],
+      selected_number: '새로 작성',
+      editNotice: false,
+      isUpdate: false, //true면 수정하기 false면 추가하기
+      editNoticeTitle: '',
+      editNoticeContent: '',
     };
   },
   components: {
@@ -236,21 +351,33 @@ export default {
     },
     setUserInfo() {
       axios
-        .post(`${SERVER_URL}/user/info`, { token: localStorage.getItem('jwt') })
+        .post(`${SERVER_URL}/user/info`, { token: this.setToken() })
         .then((response) => {
           this.userInfo.name = response.data.name;
           this.userInfo.email = response.data.email;
           this.userInfo.joindate = response.data.created_at;
           this.userInfo.number = response.data.user_number;
           this.userInfo.profileImg = response.data.profile_content;
+          this.isAdmin = response.data.admin;
           if (response.data.oauth == 'kakao') this.userInfo.sns = '카카오와 연동되어 있습니다';
           else this.userInfo.sns = '연동된 정보가 없습니다';
 
-          if (response.data.admin == true) {
-            this.userInfo.stat = '관리자';
+          if (this.isAdmin) {
+            this.userInfo.stat = '관리자';  // 관리자인 경우 notice를 수정하기 위해 notice 정보를 get으로 가져옴
+            axios
+              .get(`${SERVER_URL}/notice`)
+              .then((response) => {
+                this.notices = response.data;
+                for (let i = response.data.length - 1; i > -1; i--) {
+                  this.numbers.push(this.notices[i].notice_number);
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
           }
 
-          if (response.data.teach == true) {
+          if (response.data.teach) {
             if (response.data.admin) this.userInfo.stat += ', 교사';
             else this.userInfo.stat = '교사';
           }
@@ -260,21 +387,23 @@ export default {
           }
         })
         .catch((error) => {
-          console.log(error);
+          if (error.response.status === 401) {
+            alert('세션이 만료되었습니다.');
+            this.$emit('expireLogin');
+          } else {
+            console.log(error);
+          }
         });
     },
-
     editCancel() {
       this.newName = '';
       this.newPassword = '';
       this.newPassword2 = '';
       this.reveal = false;
     },
-
     Preview_image() {
       this.userInfo.profileImg = URL.createObjectURL(this.image);
     },
-
     updateUser() {
       if (this.newPassword != this.newPassword2) {
         alert('비밀번호가 일치하지 않습니다.');
@@ -297,14 +426,13 @@ export default {
       if (document.getElementById('photo').files[0]) {
         fd.append('photo', document.getElementById('photo').files[0]);
 
-        console.log(this.fileName);
-        console.log(fd.get('photo'));
+        // console.log(this.fileName);
+        // console.log(fd.get('photo'));
 
         if (confirm('정말 수정하시겠습니까?')) {
           axios
             .put(`${SERVER_URL}/user`, fd)
-            .then((response) => {
-              console.log(response);
+            .then(() => {
               this.setUserInfo();
 
               alert('정보 수정이 완료되었습니다!');
@@ -323,8 +451,7 @@ export default {
         if (confirm('정말 수정하시겠습니까?')) {
           axios
             .put(`${SERVER_URL}/user/noimage`, fd)
-            .then((response) => {
-              console.log(response);
+            .then(() => {
               this.setUserInfo();
 
               alert('정보 수정이 완료되었습니다!');
@@ -341,7 +468,6 @@ export default {
         }
       }
     },
-
     deleteUser() {
       if (confirm('정말 탈퇴하시겠습니까?')) {
         axios
@@ -349,7 +475,7 @@ export default {
             data: { token: localStorage.getItem('jwt') },
           })
           .then(() => {
-            alert('\n정상적으로 회원 탈퇴 되셨습니다.\n\n\n -코기는 항상 기다리고 있을게-');
+            alert('정상적으로 회원 탈퇴 되셨습니다.\n\n\n -코기는 항상 기다리고 있을게-');
 
             //자동 로그아웃해서 홈페이지로 가기
             localStorage.removeItem('jwt');
@@ -359,7 +485,7 @@ export default {
             this.$router.push({ name: 'Home' });
           })
           .catch((error) => {
-            alert('\n회원 탈퇴에 실패하셨습니다. \n\n\n 탈퇴 못하지롱\n가지마...');
+            alert('회원 탈퇴에 실패하셨습니다. \n\n\n 탈퇴 못하지롱\n가지마...');
             console.log(error);
           });
       }
@@ -391,33 +517,109 @@ export default {
 
       this.password = '';
     },
+    updateNotice() {
+      if (confirm('정말로 수정하시겠습니까?')) {
+        axios
+          .put(`${SERVER_URL}/notice`, {
+            notice_number: this.selected_number,
+            notice_title: this.editNoticeTitle,
+            notice_content: this.editNoticeContent,
+          })
+          .then(() => {
+            alert('성공적으로 글이 수정되었습니다.');
+            this.noticeList = true;
+            this.editNotice = false;
+            this.editNoticeTitle = '';
+            this.editNoticeContent = '';
+          })
+          .catch((error) => {
+            alert('error!');
+            console.log(error);
+          });
+      }
+    },
+    uploadNotice() {
+      if (this.editNoticeTitle && this.editNoticeContent) {
+        axios
+          .post(`${SERVER_URL}/notice`, {
+            admin_user_number: this.userNumber,
+            notice_title: this.editNoticeTitle,
+            notice_content: this.editNoticeContent,
+          })
+          .then(() => {
+            alert('새로운 공지 사항을 추가하시겠습니까?');
+            alert('공지 사항이 추가되었습니다.');
+            this.editNotice = false;
+            this.editNoticeTitle = '';
+            this.editNoticeContent = '';
+            this.listNotice();
+          })
+          .catch((error) => {
+            console.log(error);
+            alert('error! :(');
+          });
+      } else {
+        alert('빈 값을 공지사항에 입력할 수 없습니다.');
+      }
+    },
+    cancelNotice() {
+      if (confirm('작성중인 게시글이 삭제됩니다.\n계속 하시겠습니까?')) {
+        this.editNotice = false;
+        this.editNoticeTitle = '';
+        this.editNoticeContent = '';
+        this.selected_number = '새로 작성';
+      }
+    },
+    refreshNotice() {
+      if (!this.editNotice) {
+        this.editNoticeTitle = '';
+        this.editNoticeContent = '';
+        this.selected_number = '새로 작성';
+      }
+    },
+    changeNotice() {
+      if (this.selected_number == '새로 작성' && this.editNotice) {
+        if (confirm('글을 새로 작성하시겠습니까?\n모든 변경이 취소됩니다.')) {
+          this.isUpdate = false;
+          this.editNoticeTitle = '';
+          this.editNoticeContent = '';
+        }
+      } else {
+        for (let i = this.notices.length - 1; i > -1; i--) {
+          if (this.selected_number === this.notices[i].notice_number) {
+            if (confirm('수정 중인 내용이 제거됩니다.\n계속 하시겠습니까?')) {
+              this.isUpdate = true;
+              this.editNoticeTitle = this.notices[i].notice_title;
+              this.editNoticeContent = this.notices[i].notice_content;
+            }
+            break;
+          }
+        }
+      }
+    },
+    deleteNotice() {
+      if (confirm('\n정말로 삭제하시겠습니까?')) {
+        axios
+          .delete(`${SERVER_URL}/notice/` + this.selected.notice_number)
+          .then(() => {
+            alert('\n공지 글이 삭제되었습니다.');
+            this.noticeList = true;
+            this.listNotice();
+          })
+          .catch((error) => {
+            alert('error!');
+            console.log(error);
+          });
+      }
+    },
   },
-
   created() {
-    const userKey = {
-      token: this.setToken(),
-    };
-
-    axios
-      .post(`${SERVER_URL}/user/info`, userKey)
-      .then((response) => {
-        this.userInfo.name = response.data.name;
-        this.userInfo.email = response.data.email;
-        if (response.data.oauth) {
-          this.userInfo.sns = response.data.oauth;
-        }
-      })
-      .catch((error) => {
-        if (error.response.status === 401) {
-          alert('세션이 만료되었습니다.');
-          this.$emit('expireLogin');
-        } else {
-          console.log(error);
-        }
-      });
-
     this.setUserInfo();
   },
+  watch: {
+    selected_number: 'changeNotice',
+    editNotice: 'refreshNotice',
+  }
 };
 </script>
 
